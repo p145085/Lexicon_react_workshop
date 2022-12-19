a470400504900c1d1da3423890b45ddcd87a5f37
populus <emil.a.johansson@gmail.com>
2022-12-19 10:10:33 +0100

    Add project files.

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Lexicon_react_workshop
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
